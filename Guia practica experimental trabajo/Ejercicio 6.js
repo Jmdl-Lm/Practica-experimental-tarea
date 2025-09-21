def7512a774f73039e33e543281a56e3d3e62c61
Jmdl-Lm <jmuriell@unemi.edu.ec>
2025-09-20 21:22:12 -0500

    Ejercicio 6.js

--- Guia practica experimental trabajo/Ejercicio 6.js
+++ Guia practica experimental trabajo/Ejercicio 6.js
@@ -24,7 +24,7 @@ for(let i = 0; i < pedidos; i++){
     cantidadTarjetas[i] = parseInt(prompt(`Ingrese la cantidad de tarjetas del pedido ${N}`));
     if (cantidadTarjetas[i] <= 200) {
         precio = 2.00;
-    } else if (cantidadTarjetas[i] >= 201 & cantidadTarjetas[i] <= 300) {
+    } else if (cantidadTarjetas[i] >= 201 && cantidadTarjetas[i] <= 300) {
         precio = 1.80;
     } else {
         precio = 1.50;
@@ -32,4 +32,5 @@ for(let i = 0; i < pedidos; i++){
     vf[i] = cantidadTarjetas[i] * precio;
     alert(`El costo total del pedido ${N} es $${vf[i]}`);
     N++
+
 }
